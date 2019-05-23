29695e56d22f944377d9386c8d23106edd324e31
DanLaZ <lazenberry_daniel@yahoo.com>
2019-05-23 16:03:40 -0400

    Clock resets properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,63 +6,53 @@
 var time = 120;
 var clockRunning = false;
 var intervalId;
-//
-var stopAtZero = setTimeout(stop, (2 * 60 * 1000) + 2000 );
 
 window.onload = function() {
 
-    $("#startTimer").on("click", start);
-    $("#startTimer").on("click", stopAtZero);
+  $("#startTimer").on("click", start);
+  $("#startTimer").on("click", stop);
 }
 
 function start() {
 
-    if (!clockRunning) {
-      intervalId = setInterval(count, 1000);
-      clockRunning = true;
-    }
+  if (!clockRunning) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
+  }
 }
 
 function stop() {
 
-    if (time === 0) {
-        clearInterval(intervalId);
-        clockRunning = false;
-    }
+  setTimeout(function() {
+    clearInterval(intervalId);
+    clockRunning = false;
     alert("Time's up!");
+    window.location.reload();
+  }, 120000)
 }
 
 function count() {
 
-    time--;
-    var converted = timeConverter(time);
-    $("#timerCount").text(converted);
+  time--;
+  var converted = timeConverter(time);
+  $("#timerCount").text(converted);
 }
 
 function timeConverter(t) {
 
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-  
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-  
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-  
-    return minutes + ":" + seconds;
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return minutes + ":" + seconds;
+
 }
-
-
-/* SetTime Function */
-var countDown = setTimeout(postResults, (2 * 60 * 1000) + 2000 ); //<=== set for 2mins
-$("#startTimer").on("click", countDown);
-
-/*  */
-
-
